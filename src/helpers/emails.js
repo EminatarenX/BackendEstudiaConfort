b3eaef29001e5684c949f46b44e2bdd7d88acebc
EminatarenX <eminataren2002@gmail.com>
2023-07-19 13:28:50 -0600

    Actualizacion de mailtrap a gmail para enviar correos

--- src/helpers/emails.js
+++ src/helpers/emails.js
@@ -1,34 +1,37 @@
 const nodemailer = require('nodemailer')
 
-const emailRegistro = async(datos) => {
-  
-  const {correo, nombre, token} = datos
+const emailRegistro = async (datos) => {
+
+  const { correo, nombre, token } = datos
 
 
   const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    service: "gmail",
     auth: {
-      user: "5c73a16264e257",
-      pass: "b0b35da0d73cce"
+      user: "eminataren2002@gmail.com",
+      pass: "mllyxowmntwaberp"
     }
   });
 
 
   //informacion del email
   const info = await transport.sendMail({
-    from: '"EstudiaConfort - Conoce tu nueva habitacion" <cuentas@estudiaconfort.com>',
+    from: '"EstudiaConfort - Conoce tu nueva habitación" <cuentas@estudiaconfort.com>',
     to: correo,
     subject: 'EstudiaConfort - Confirma tu cuenta!',
     text: 'Bienvenido a Estudia Confort, confirma tu cuenta',
-    html: ` <div style="padding: 40px 30px; display: flex; flex-direction: column; justify-content: center;gap: 20px; background: linear-gradient(135deg, rgba(41,73,94,1) 0%, rgba(0,21,74,1) 100%);">
-    <h1 style="font-weigth: 700; font-size: 30px; color: white;">Tu cuenta esta casi lista! solo debes de comprobarla dando click en el siguiente enlace:</h1>
+    html: `
+        <div style="padding: 40px 30px; background: linear-gradient(135deg, rgba(41,73,94,1) 0%, rgba(0,21,74,1) 100%);">
+          <h1 style="font-weight: 700; font-size: 30px; color: white; margin-bottom: 20px;">Tu cuenta está casi lista. ¡Solo debes comprobarla haciendo clic en el siguiente enlace:</h1>
+        
+          <div style="display: flex; flex-direction: column; gap: 10px;">
+              <a style="color: white;" href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar cuenta</a>
 
-    <a style="color: white;" href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar cuenta</a>
+                <p style="color: white; margin: 0;">Si no creaste esta cuenta, ignora este mensaje.</p>
+          </div>
+        </div>
+        `
 
-    <p style="color: white" >Si no creaste esta cuenta ignora este mensaje</p>
-    </div>
-    `
   })
 }
 
